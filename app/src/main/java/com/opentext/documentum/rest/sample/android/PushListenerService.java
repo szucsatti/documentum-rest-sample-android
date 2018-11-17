1e43feebeb6c32dec7681fa1e3f0d95f92c5dc46
szucsatti <szucsatti87@gmail.com>
2018-11-17 15:23:25 +0200

    push listener updated

--- app/src/main/java/com/opentext/documentum/rest/sample/android/PushListenerService.java
+++ app/src/main/java/com/opentext/documentum/rest/sample/android/PushListenerService.java
@@ -1,5 +1,6 @@
 package com.opentext.documentum.rest.sample.android;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
@@ -35,44 +36,21 @@ public class PushListenerService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
         Log.d(TAG, "This is the message: " + remoteMessage.getData());
 
-        final NotificationClient notificationClient = MainActivity.getPinpointManager(getApplicationContext()).getNotificationClient();
+        final HashMap<String, String> dataMap = new HashMap<>(remoteMessage.getData());
+        broadcast(remoteMessage.getFrom(), dataMap);
 
-        final NotificationDetails notificationDetails = NotificationDetails.builder()
-                .from(remoteMessage.getFrom())
-                .mapData(remoteMessage.getData())
-                .intentAction(NotificationClient.FCM_INTENT_ACTION)
-                .build();
-
-        NotificationClient.CampaignPushResult pushResult = notificationClient.handleCampaignPush(notificationDetails);
-
-        if (!NotificationClient.CampaignPushResult.NOT_HANDLED.equals(pushResult)) {
-            /**
-             The push message was due to a Pinpoint campaign.
-             If the app was in the background, a local notification was added
-             in the notification center. If the app was in the foreground, an
-             event was recorded indicating the app was in the foreground,
-             for the demo, we will broadcast the notification to let the main
-             activity display it in a dialog.
-             */
-            if (NotificationClient.CampaignPushResult.APP_IN_FOREGROUND.equals(pushResult)) {
-                /* Create a message that will display the raw data of the campaign push in a dialog. */
-                final HashMap<String, String> dataMap = new HashMap<>(remoteMessage.getData());
-                broadcast(remoteMessage.getFrom(), dataMap);
-            }
-            return;
-        }
     }
 
     private void broadcast(final String from, final HashMap<String, String> dataMap) {
         Intent intent = new Intent(ACTION_PUSH_NOTIFICATION);
         intent.putExtra(INTENT_SNS_NOTIFICATION_FROM, from);
         intent.putExtra(INTENT_SNS_NOTIFICATION_DATA, dataMap);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        sendBroadcast(intent);
     }
 
     /**
      * Helper method to extract push message from bundle.
-     *
+     * sendBroadcast(intent);
      * @param data bundle
      * @return message string from push notification
      */
